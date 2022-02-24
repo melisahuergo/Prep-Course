f1541060472bc61eddade3a3772b6cd99b907ceb
melisa huergo <melisahuergo-coquetos@outlook.es>
2022-02-24 17:24:16 -0300

    practicando

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "1234mmm";
+const nuevaString = "xxxx";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
